955855ad982868055589718fd86eaa24105e7f1a
EliavetaIvanova <121135778+EliavetaIvanova@users.noreply.github.com>
2022-12-21 02:12:22 +0300

    Add files via upload

---
+++ LinkArr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor
+{
+    // класс, связывающий стрелки с окном
+    class LinkArr
+    {
+        // указывает какая сторона стрелки связана с окном
+        bool indexSide;
+        // указывает какая стрлка связана с окном
+        Arrow line;
+        // указывает какая сторона окна связана со стрелкой
+        char location;
+
+        // конструктор класса
+        public LinkArr(bool indexSide, Arrow line, char location)
+        {
+            this.indexSide = indexSide;
+            this.line = line;
+            this.location = location;
+        }
+
+        // геттер
+        public bool getIndesSide()
+        {
+            return this.indexSide;
+        }
+
+        // геттер
+        public Arrow getLine()
+        {
+            return this.line;
+        }
+
+        // геттер
+        public char getLocation()
+        {
+            return this.location;
+        }
+    }
+}
